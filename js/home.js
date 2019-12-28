a3a18770c90acb34f4aff4245b41993ff9fc69dd
Vaibhav Pathak <45169573+vkpdeveloper@users.noreply.github.com>
2019-12-28 16:13:21 +0530

    Update 2.0

--- js/home.js
+++ js/home.js
@@ -1,3 +1,18 @@
+function deleteInvoice(id) {
+    let db = firebase.firestore();
+    let data = JSON.parse(localStorage.getItem('login'));
+    let invoiceRef = db.collection(data[0]);
+    let query = invoiceRef.where('invoiceNo', '==', id);
+    query.get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          doc.ref.delete();
+        });
+        setTimeout(()=>{
+            window.location.reload();
+        }, 500)
+      });
+}
+
 if (localStorage.getItem('login') == null) {
     window.location.href = '/';
 } else {
@@ -23,24 +38,34 @@ if (localStorage.getItem('login') == null) {
         let query = userRef.where('invoiceNo', '>=', '1');
         let tableData = ``;
         query.get().then(bills => {
-            bills.forEach(bill => {
-                let everyBill = bill.data();
-                tableData += `<tr>
-                <th scope="row">${everyBill.invoiceNo}</th>
-                <td>${everyBill.date}</td>
-                <td class="customer">${everyBill.customer}</td>
-                <td>${everyBill.address}</td>
-                <td>${everyBill.pcs}</td>
-                <td>${everyBill.productName}</td>
-                <td>${everyBill.unitPrice}</td>
-                <td>${everyBill.totalPrice}</td>
-                <td>${everyBill.mobile}</td>
-                </tr>`
-            })
-            let tbody = document.getElementsByTagName('tbody')[0];
-            tbody.innerHTML = tableData;
+            if (bills.size != 0) {
+                bills.forEach(bill => {
+                    let everyBill = bill.data();
+                    tableData += `<tr>
+                    <th scope="row">${everyBill.invoiceNo}</th>
+                    <td>${everyBill.date}</td>
+                    <td class="customer">${everyBill.customer}</td>
+                    <td>${everyBill.address}</td>
+                    <td>${everyBill.pcs}</td>
+                    <td>${everyBill.productName}</td>
+                    <td>${everyBill.unitPrice}</td>
+                    <td>${everyBill.totalPrice}</td>
+                    <td>${everyBill.mobile}</td>
+                    <td><button onclick="deleteInvoice(this.id)" type="button" class="btn btn-danger" id=${everyBill.invoiceNo}><i class="fa fa-trash"></i></td>
+                    </tr>`
+                })
+                let tbody = document.getElementsByTagName('tbody')[0];
+                tbody.innerHTML = tableData;
+            } else {
+                let tableData = `<p class="text-center" >No Invoices Found !`;
+                let tbody = document.getElementById('noBill');
+                tbody.innerHTML = tableData;
+            }
+
         }).catch(err => {
-            console.log(err);
+            let tableData = `<p>No Invoices Found ! Error : ${err}`;
+            let tbody = document.getElementById('noBill');
+            tbody.innerHTML = tableData;
         })
 
     });
